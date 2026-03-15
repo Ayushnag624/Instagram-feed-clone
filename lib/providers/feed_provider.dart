4deca1b104f129768743df0fad46c20bb72e3ee4
ayushnag624 <ayushnag12321@gmail.com>
2026-03-15 23:24:36 +0530

    instagram feed clone

---
+++ lib/providers/feed_provider.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import '../models/post_model.dart';
+import '../services/post_repository.dart';
+
+class PostProvider extends ChangeNotifier {
+
+  final PostRepository repository = PostRepository();
+
+  List<Post> posts = [];
+
+  bool isLoading = false;
+  int page = 0;
+
+  Future<void> loadPosts() async {
+
+    if (isLoading) return;
+
+    isLoading = true;
+    notifyListeners();
+
+    final newPosts = await repository.fetchPosts(page);
+
+    posts.addAll(newPosts);
+    page++;
+
+    isLoading = false;
+    notifyListeners();
+  }
+}
